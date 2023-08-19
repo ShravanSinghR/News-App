4ff9fa9601e998389bea23e19de0c2e10479f968
Shravan singh <06shravansingh@gmail.com>
2023-08-20 00:05:02 +0530

    changes in Material

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -10,11 +10,11 @@ export class NewsItem extends Component {
                     <img src={imageUrl ? imageUrl : "https://cdn.friendsoftheearth.uk/sites/default/files/styles/hero_image/public/media/images/art-attachment-background-boat-40906.jpeg?itok=mQP6NcPZ"} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}<span className="position-absolute top-0  translate-middle badge rounded-pill "
-                        style={{left:"50%",zIndex:"1" , backgroundColor: this.props.material}}>
+                        style={{left:"50%",zIndex:"1" , backgroundColor:material}}>
                             {source}
                         </span></h5>
                         <p className="card-text">{description}.</p>
-                        <a href={newsUrl} target='_blank' className="btn btn-sm btn-warning">Read More</a>
+                        <a href={newsUrl} target='_blank'  rel="noreferrer" className="btn btn-sm btn-warning">Read More</a>
                         <p className="card-text mt-2 "><small className="text-body-secondary">Publiished at {new Date(date).toGMTString()}</small></p>
                     </div>
                 </div>
